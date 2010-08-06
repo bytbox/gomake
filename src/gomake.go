7e2db697bc25bfc0656d75b3a7a312bc272590db
Scott Lawrence <bytbox@gmail.com>
2010-08-05 20:21:02 -0400

    fixed gomake to act like make where appropriate, but not when only options are given

--- src/gomake.go
+++ src/gomake.go
@@ -12,15 +12,21 @@ var showVersion = opts.Longflag("version", "display version information")
 var outputFilename = opts.Shortopt("o", "file to write makefile to", "Makefile")
 
 func main() {
+	// if any arguments were given, this is being used as 'make'
+	asMake := false
+	for num, arg := range os.Args {
+		if arg[0] != '-' && num != 0 {
+			asMake = true
+		}
+	}
+	if asMake {
+		make, _ := exec.LookPath("make")
+		os.Exec(make,os.Args,nil)
+	}
 	// parse and handle options
 	opts.Parse()
 	if *showVersion {
 		ShowVersion()
 		os.Exit(0)
-	}
-	// if any arguments were given, this is being used as 'make'
-	if len(opts.Args) > 0 {
-		make, _ := exec.LookPath("make")
-		os.Exec(make,os.Args,nil)
 	}
 }
