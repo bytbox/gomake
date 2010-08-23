586539030d046a871f2b3f33afe0c65b7c1e6526
Scott Lawrence <bytbox@gmail.com>
2010-08-23 07:30:31 -0400

    added LICENSE notice to --version (linking to http://golang.org/LICENSE)

--- src/common.go
+++ src/common.go
@@ -18,6 +18,12 @@ var version = "0.2.3"
 // Show version information
 func ShowVersion() {
 	fmt.Printf("%s (GoMake) v%s\n", progName, version)
+	fmt.Print(
+`
+Copyright 2010 The Go Authors. All rights reserved.
+Use is governed by a BSD-style license that can be found in 
+the LICENSE file, or online at http://golang.org/LICENSE.
+`)
 }
 
 func ReadConfig(filename string) (map[string]string, os.Error) {
